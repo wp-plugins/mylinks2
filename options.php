924de854abba2dbe990946785369d8525a7597de
2020media <2020media@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-11-11 17:46:49 +0000

    add new thumbnail generation service
    git-svn-id: https://plugins.svn.wordpress.org/mylinks2/trunk@461906 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- options.php
+++ options.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Donate link: http://www.2020media.com/wordpress
-New version (June 2011) of options page using new API
+New version (November 2011) of options page offers choice of thumbnail API
 ref: http://ottopress.com/2009/wordpress-settings-api-tutorial/
 ref: http://www.presscoders.com/wordpress-settings-api-explained/
 */
@@ -32,30 +32,37 @@ add_action('admin_init', 'plugin_admin_init');
 function plugin_admin_init(){
 register_setting( 'plugin_options', 'mylinks2_plugin_options', 'plugin_options_validate' );
 add_settings_section('plugin_main', 'Main Settings', 'plugin_section_text', 'mylinks2');
-add_settings_field('plugin_api_key', 'API Key', 'plugin_setting_api_key', 'mylinks2', 'plugin_main');
+add_settings_field('plugin_api_service', 'Thumbnail Service', 'plugin_setting_api_service', 'mylinks2', 'plugin_main');
+add_settings_field('plugin_api_key', 'ShrinkTheWeb API Key*', 'plugin_setting_api_key', 'mylinks2', 'plugin_main');
 add_settings_field('plugin_img_size', 'Image Size', 'plugin_setting_img_size', 'mylinks2', 'plugin_main');
-add_settings_field('plugin_thumb_layout', 'Image Layout', 'plugin_setting_thumb_layout', 'mylinks2', 'plugin_main');
+add_settings_field('plugin_thumb_layout', 'Page Layout', 'plugin_setting_thumb_layout', 'mylinks2', 'plugin_main');
 add_settings_section('plugin_desc', 'Help', 'plugin_section_text2', 'mylinks2');
 
 }
 
 function plugin_section_text() {
-echo '<p>Please enter your API key, and choose layout options. Click Save Changes once done.</p>';
+echo '<p>Please choose your options. API key is only necessary for ShrinkTheWeb. Click Save Changes once done.</p>';
 }
+
 
 function plugin_section_text2() {
-echo '<p>MyLinks2 uses the website thumbnail service from <a href=http://www.shrinktheweb.com target=_blank>ShrinkTheWeb</a>. You will need to obtain an <a href=http://www.shrinktheweb.com/user/register target=_blank>API key</a>.<br /> The basic service offers 5,000 image hits per month and it\'s free. If you have a busy site, you can <a href=http://www.shrinktheweb.com/content/compare-thumbnail-offerings.html target=_blank>upgrade</a> your API key.
-<br /><br />Need to find your API key? See <a href=http://www.shrinktheweb.com/content/where-can-i-find-my-api-key.html target=_blank>http://www.shrinktheweb.com/content/where-can-i-find-my-api-key.html</a></p><p><p>&nbsp;</p><em>Please note MyLinks2 and it\'s creators are not affiliated with ShrinkTheWeb in any way.</em></p>';
+echo '<p>*MyLinks2 offers the choice of two thumbnail generation services. These are <a href=http://pagepeeker.com target=_blank>PagePeeker.com</a> and <a href=http://www.shrinktheweb.com target=_blank>ShrinkTheWeb</a>.<br />Only with ShrinkTheWeb will you need to obtain an <a href=http://www.shrinktheweb.com/user/register target=_blank>API key</a>.<br /> The basic service from both providers is free.
+<br /><br />Need to find your API key with ShrinkTheWeb? See <a href=http://www.shrinktheweb.com/content/where-can-i-find-my-api-key.html target=_blank>http://www.shrinktheweb.com/content/where-can-i-find-my-api-key.html</a></p><p><p>&nbsp;</p><em>Please note MyLinks2 and it\'s creators are not affiliated with PagePeeker.com or ShrinkTheWeb in any way.</em></p>';
 }
 
-
-function plugin_setting_api_key() {
+function plugin_setting_api_service() {
+$items = array("STW"=>"ShrinkTheWeb", "PP"=>"PagePeeker");
 $options = get_option('mylinks2_plugin_options');
-echo "<input id='plugin_api_key' name='mylinks2_plugin_options[api_key]' size='40' type='text' value='{$options['api_key']}' />";
+echo "<select id='plugin_api_service' name='mylinks2_plugin_options[api_service]'>";
+	foreach($items as $item=>$itemnice) {
+		$selected = ($options['api_service']==$item) ? 'selected="selected"' : '';
+		echo "<option value='$item' $selected>$itemnice</option>";
+	}
+	echo "</select>";
 }
 
 function plugin_setting_img_size() {
-$items = array("mcr"=>"Micro 75x56", "tny"=>"Tiny 90x68", "vsm"=>"Very small 100x75", "sm"=>"Small 120x90", "lg"=>"Large 200x150", "xlg"=>"Extra Large 320x240");
+$items = array("mcr"=>"Micro", "tny"=>"Tiny", "vsm"=>"Small", "sm"=>"Medium", "lg"=>"Large", "xlg"=>"Extra Large");
 $options = get_option('mylinks2_plugin_options');
 echo "<select id='plugin_img_size' name='mylinks2_plugin_options[img_size]'>";
 	foreach($items as $item=>$itemnice) {
@@ -65,6 +72,10 @@ echo "<select id='plugin_img_size' name='mylinks2_plugin_options[img_size]'>";
 	echo "</select>";
 }
 
+function plugin_setting_api_key() {
+$options = get_option('mylinks2_plugin_options');
+echo "<input id='plugin_api_key' name='mylinks2_plugin_options[api_key]' size='40' type='text' value='{$options['api_key']}' />";
+}
 
 function plugin_setting_thumb_layout() {
 //$items = array("Left", "Right");
@@ -90,6 +101,10 @@ $options['img_size'] = trim($input['img_size']);
 if(!preg_match('/^[a-z0-9]{2,}$/i', $options['img_size'])) {
 $options['img_size'] = 'error';
 }
+$options['api_service'] = trim($input['api_service']);
+if(!preg_match('/^[a-z0-9]{2,}$/i', $options['api_service'])) {
+$options['api_service'] = 'error';
+}
 $options['thumb_layout'] = trim($input['thumb_layout']);
 if(!preg_match('/^[a-z0-9]+$/i', $options['thumb_layout'])) {
 $options['thumb_layout'] = 'error';
@@ -97,6 +112,7 @@ $options['thumb_layout'] = 'error';
 
 return $options;
 }
+
 
 
 
